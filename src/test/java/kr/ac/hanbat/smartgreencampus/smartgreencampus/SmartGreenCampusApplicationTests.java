c724fb9f2e7d07abd247ab4eb867dfc2bff95e97
leedongyeop <ldy_1204@naver.com>
2023-03-16 12:57:18 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/kr/ac/hanbat/smartgreencampus/smartgreencampus/SmartGreenCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hanbat.smartgreencampus.smartgreencampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGreenCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
